4dd4cbddf8cff141e51d90b1851f3496ef93a8f4
James McKinney <james@slashpoundbang.com>
2013-09-26 20:32:05 -0400

    test memcached integration

--- lib/pupa/processor/client.rb
+++ lib/pupa/processor/client.rb
@@ -18,7 +18,10 @@ module Pupa
     class Client
       # Returns a configured Faraday HTTP client.
       #
-      # @param [String] cache_dir a directory in which to cache requests
+      # In order to automatically parse XML responses, you must `require 'multi_xml'`.
+      #
+      # @param [String] cache_dir a directory or a Memcached address
+      #   (e.g. memcached://localhost:11211) in which to cache requests
       # @param [Integer] expires_in the cache's expiration time in seconds
       # @param [String] level the log level
       # @return [Faraday::Connection] a configured Faraday HTTP client
@@ -26,15 +29,19 @@ module Pupa
         Faraday.new do |connection|
           connection.request :url_encoded
           connection.use Middleware::Logger, Logger.new('faraday', level: level)
+
           # @see http://tools.ietf.org/html/rfc2854
           # @see http://tools.ietf.org/html/rfc3236
           connection.use Middleware::ParseHtml, content_type: %w(text/html application/xhtml+xml)
+
           # @see http://tools.ietf.org/html/rfc4627
           connection.use FaradayMiddleware::ParseJson, content_type: /\bjson$/
-          # @see http://tools.ietf.org/html/rfc3023
+
           if defined?(MultiXml)
+            # @see http://tools.ietf.org/html/rfc3023
             connection.use FaradayMiddleware::ParseXml, content_type: /\bxml$/
           end
+
           if cache_dir
             connection.response :caching do
               address = cache_dir[%r{\Amemcached://(.+)\z}, 1]
@@ -45,6 +52,7 @@ module Pupa
               end
             end
           end
+
           connection.adapter Faraday.default_adapter # must be last
         end
       end
--- spec/processor/client_spec.rb
+++ spec/processor/client_spec.rb
@@ -2,6 +2,14 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Pupa::Processor::Client do
   describe '.new' do
+    it 'should use the filesystem' do
+      ActiveSupport::Cache::FileStore.should_receive(:new).and_call_original
+      Pupa::Processor::Client.new(cache_dir: '/tmp', level: 'UNKNOWN').get('http://example.com/')
+    end
 
+    it 'should use Memcached' do
+      ActiveSupport::Cache::MemCacheStore.should_receive(:new).and_call_original
+      Pupa::Processor::Client.new(cache_dir: 'memcached://localhost', level: 'UNKNOWN').get('http://example.com/')
+    end
   end
 end
