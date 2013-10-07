f679bfd5a8a979d2e881db969d0ef243846d2474
James McKinney <james@slashpoundbang.com>
2013-10-06 21:20:15 -0400

    add comment

--- lib/pupa/models/concerns/timestamps.rb
+++ lib/pupa/models/concerns/timestamps.rb
@@ -15,6 +15,7 @@ module Pupa
         end
 
         set_callback(:save, :before) do |object|
+          # The object may not set created_at.
           object.created_at = object.document['created_at'] if object.document
           object.updated_at = Time.now.utc
         end
