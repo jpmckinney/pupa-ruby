6ba5818418c22f8cf7a3798885460cf625c6d044
James McKinney <james@slashpoundbang.com>
2014-10-10 22:08:07 -0400

    add group_results and counts to VoteEvent

--- lib/pupa/models/vote_event.rb
+++ lib/pupa/models/vote_event.rb
@@ -8,16 +8,65 @@ module Pupa
     include Concerns::Timestamps
     include Concerns::Sourceable
 
-    attr_accessor :identifier, :motion_id, :organization_id, :legislative_session_id, :start_date, :end_date, :result
-    dump          :identifier, :motion_id, :organization_id, :legislative_session_id, :start_date, :end_date, :result
+    attr_accessor :identifier, :motion_id, :organization_id, :legislative_session_id, :start_date, :end_date, :result, :group_results, :counts
+    dump          :identifier, :motion_id, :organization_id, :legislative_session_id, :start_date, :end_date, :result, :group_results, :counts
 
     foreign_key :motion_id, :organization_id, :legislative_session_id
+
+    def initialize(*args)
+      @group_results = []
+      @counts = []
+      super
+    end
 
     # Returns the vote event's identifier and organization ID.
     #
     # @return [String] the vote event's identifier and organization ID
     def to_s
       "#{identifier} in #{organization_id}"
+    end
+
+    # Sets the group results.
+    #
+    # @param [Array] group_results a list of group results
+    def group_results=(group_results)
+      @group_results = symbolize_keys(group_results)
+    end
+
+    # Sets the counts.
+    #
+    # @param [Array] counts a list of counts
+    def counts=(counts)
+      @counts = symbolize_keys(counts)
+    end
+
+    # Adds a group result.
+    #
+    # @param [String] result the result of the vote event within a group of voters
+    # @param [String] group a group of voters
+    def add_group_result(result, group: nil)
+      data = {result: result}
+      if group
+        data[:group] = group
+      end
+      if result.present?
+        @group_results << data
+      end
+    end
+
+    # Adds a count.
+    #
+    # @param [String] option an option in a vote event
+    # @param [String] value the number of votes for an option
+    # @param [String] group a group of voters
+    def add_count(option, value, group: nil)
+      data = {option: option, value: value}
+      if group
+        data[:group] = group
+      end
+      if option.present? && value.present?
+        @counts << data
+      end
     end
   end
 end
--- spec/models/vote_event_spec.rb
+++ spec/models/vote_event_spec.rb
@@ -10,4 +10,50 @@ describe Pupa::VoteEvent do
       object.to_s.should == '1 in legislative-council-of-hong-kong'
     end
   end
+
+  describe '#group_results' do
+    it 'should symbolize keys' do
+      object.group_results = [{'result' => 'pass', 'group' => {'name' => 'Functional constituencies'}}]
+      object.group_results.should == [{result: 'pass', group: {name: 'Functional constituencies'}}]
+    end
+  end
+
+  describe '#add_group_result' do
+    it 'should add a group result' do
+      object.add_group_result('pass', group: {name: 'Functional constituencies'})
+      object.group_results.should == [{result: 'pass', group: {name: 'Functional constituencies'}}]
+    end
+
+    it 'should not add a group result without a result' do
+      object.add_group_result(nil)
+      object.add_group_result('')
+      object.group_results.blank?.should == true
+    end
+  end
+
+  describe '#counts' do
+    it 'should symbolize keys' do
+      object.counts = [{'option' => 'yes', 'value' => 9, 'group' => {'name' => 'Functional constituencies'}}]
+      object.counts.should == [{option: 'yes', value: 9, group: {name: 'Functional constituencies'}}]
+    end
+  end
+
+  describe '#add_count' do
+    it 'should add a count' do
+      object.add_count('yes', 9, group: {name: 'Functional constituencies'})
+      object.counts.should == [{option: 'yes', value: 9, group: {name: 'Functional constituencies'}}]
+    end
+
+    it 'should not add a contact detail without an option' do
+      object.add_count(nil, 9)
+      object.add_count('', 9)
+      object.counts.blank?.should == true
+    end
+
+    it 'should not add a contact detail without a value' do
+      object.add_count('yes', nil)
+      object.add_count('yes', '')
+      object.counts.blank?.should == true
+    end
+  end
 end
