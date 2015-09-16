c2ad031bbc6c41bd95215b9541e2e991184de428
James McKinney <james@slashpoundbang.com>
2015-09-16 00:17:02 -0400

    get tests passing in other rubies

--- spec/processor/connection_adapters/sqlite_adapter_spec.rb
+++ spec/processor/connection_adapters/sqlite_adapter_spec.rb
@@ -1,5 +1,5 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe Pupa::Processor::Connection::PostgreSQLAdapter do
-  include_examples 'SQL adapter', 'sqlite://pupa_test.db'
+  include_examples 'SQL adapter', 'sqlite://test.db'
 end
