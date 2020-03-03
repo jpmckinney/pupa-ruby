b0d5d0a10e7653cdaa260f06c4c8f31ef181058c
James McKinney <26463+jpmckinney@users.noreply.github.com>
2020-03-03 13:22:14 -0500

    Update test to use existing database

--- spec/processor/connection_spec.rb
+++ spec/processor/connection_spec.rb
@@ -3,13 +3,13 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe Pupa::Processor::Connection do
   describe '.new' do
     it 'should use MongoDB' do
-      expect(Pupa::Processor::Connection::MongoDBAdapter).to receive(:new).with('mongodb://localhost:27017/pupa').and_call_original
-      Pupa::Processor::Connection.new('mongodb://localhost:27017/pupa')
+      expect(Pupa::Processor::Connection::MongoDBAdapter).to receive(:new).with('mongodb://localhost:27017/pupa_test').and_call_original
+      Pupa::Processor::Connection.new('mongodb://localhost:27017/pupa_test')
     end
 
     it 'should use PostgreSQL' do
-      expect(Pupa::Processor::Connection::PostgreSQLAdapter).to receive(:new).with('postgres://localhost:5432/pupa').and_call_original
-      Pupa::Processor::Connection.new('postgres://localhost:5432/pupa')
+      expect(Pupa::Processor::Connection::PostgreSQLAdapter).to receive(:new).with('postgres://localhost:5432/pupa_test').and_call_original
+      Pupa::Processor::Connection.new('postgres://localhost:5432/pupa_test')
     end
   end
 end
